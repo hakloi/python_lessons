b457fa3097c67c4d7461701289a8f700377d5cc9
omezhka <murasakitta@gmail.com>
2022-10-25 15:40:21 +0300

    Урок 3

---
+++ seminar/lesson3/ex1.py
@@ -0,0 +1,22 @@
+from itertools import count
+import os
+clear = lambda: os.system('cls')
+clear()
+
+count_p = 0
+count_o = 0
+
+text = input("Введите произвольную последовательность букв О и Р: ")
+
+for char in text:
+    if char == "Р":
+        count_p += 1
+    elif char == "О":
+        count_o += 1
+    else:
+        print('Неправильно введеные данные')
+print('Решка: {}'.format(count_p))
+print('Орел: {}'.format(count_o))
+
+if count_p == 0:
+    print('\nРешек нет...')
