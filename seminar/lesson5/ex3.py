667ba4f37f9d365449e4b05848760e4e9f7087ba
omezhka <murasakitta@gmail.com>
2022-11-06 16:08:05 +0300

    Список b+c

---
+++ seminar/lesson5/ex3.py
@@ -0,0 +1,9 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+
+a = [4, 3, -10, 1, 7, 12]
+b = [i for i in a if i%2==0]
+c = [i for i in a if i%2!=0]
+print(f'Первоначальный список: {a}')
+print(f'Новый список b+c: {b+c}')
