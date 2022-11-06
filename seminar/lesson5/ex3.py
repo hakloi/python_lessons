a3dd188e2d3184e0a2945488aa7e76e54dad4173
omezhka <murasakitta@gmail.com>
2022-11-06 16:15:39 +0300

    sort метод

--- seminar/lesson5/ex3.py
+++ seminar/lesson5/ex3.py
@@ -3,7 +3,9 @@ clear = lambda: os.system('cls')
 clear()
 
 a = [4, 3, -10, 1, 7, 12]
-b = [i for i in a if i%2==0]
-c = [i for i in a if i%2!=0]
-print(f'Первоначальный список: {a}')
-print(f'Новый список b+c: {b+c}')
+# b = [i for i in a if i%2==0]
+# c = [i for i in a if i%2!=0]
+# print(f'Первоначальный список: {a}')
+# print(f'Новый список b+c: {b+c}')
+a.sort(key=lambda x: x%2)
+print(a)
