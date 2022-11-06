3d5091e2d7ccb1477c3d194107716043170bbf4e
omezhka <murasakitta@gmail.com>
2022-11-06 15:52:17 +0300

    Строка в zip

--- seminar/lesson5/ex1.py
+++ seminar/lesson5/ex1.py
@@ -2,3 +2,7 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
+text = list(map(str, input('Введите строку: ').strip()))
+zipper = [i for i in range(11)]
+result = list(zip(text, zipper))
+print(*result)
